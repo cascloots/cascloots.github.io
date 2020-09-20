64ff6108f829b34e487e13faa0b7df88090c341e
Cas Cloots <cloots.cas@gmail.com>
2020-09-20 19:45:29 +0200

    ✨ Slider

---
+++ app.js
@@ -0,0 +1,15 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY)+"px; left: "+(e.pageX)+"px;")
+})
+
+document.addEventListener('mousedown', () => {
+  cursor.classList.add("expand");
+});
+
+document.addEventListener('mouseup', () => {
+  cursor.classList.remove("expand");
+});
+
+
