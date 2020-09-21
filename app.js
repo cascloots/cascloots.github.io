8ea0cd4e4b5036b2b0a4e6c7e56ddf2ad953b321
Cas Cloots <cloots.cas@gmail.com>
2020-09-21 14:02:15 +0200

    📚 Added comments

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const cursor = document.querySelector('.cursor');
 let navLinks = document.querySelectorAll('.navigation li')
 
+/** track cursor aand apply style */
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY)+"px; left: "+(e.pageX)+"px;")
 })
 
+/** animation on click */
 document.addEventListener('mousedown', () => {
   cursor.classList.add("expand");
 });
@@ -13,6 +15,7 @@ document.addEventListener('mouseup', () => {
   cursor.classList.remove("expand");
 });
 
+/** change cursor when hovering links */
 navLinks.forEach(link =>{
   link.addEventListener("mouseleave", () => {
     document.getElementById("cursor").innerHTML = "READ";
