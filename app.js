0ce3d186703306e01c68dc955d0f3d5e91c694fe
Cas Cloots <cloots.cas@gmail.com>
2020-10-12 20:32:08 +0200

    🎨 Content styling

--- app.js
+++ app.js
@@ -28,6 +28,32 @@ let navLinks = document.querySelectorAll('.navigation li');
 //   });
 // });
 
+var message = "👉 Come back we miss you";
+var original = document.title;
+
+window.onblur = function () { document.title = message; }
+window.onfocus = function () { document.title = original; }
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 120px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 /** load json data */
 const loadJSON = (file, callback) => {
   const xobj = new XMLHttpRequest();
@@ -71,17 +97,25 @@ const element = ({ type, className, content, src, alt, children }) => {
 const generateCases = cases => cases.map(( item, index ) => {
   getElement({ id: 'cases', children: [
     element({ type: 'div', className: index % 2 ? 'odd' : 'even', children: [
-      element({ type: 'h2', className: 'case-category', content: `↓ ${item.category} ↓` }),
-      element({ type: 'h1', className: 'case-title', content: `${item.client} - ${item.title}` }),
+      element({ type: 'div', className: 'case-header', children: [
+        element({ type: 'div', className: 'content', children: [
+        element({ type: 'h2', className: 'case-category', content: `↓ ${item.category} ↓` }),
+        element({ type: 'h1', className: 'case-title', content: `${item.client} - ${item.title}` }),
+        ] }),
+      ] }),
       element({ type: 'div', className: 'main-carousel', children: item.carousel.map(image => (
         element({ type: 'img', className: 'carousel-cell', src: image.src, alt: image.alt })
       ))}),
-      element({ type: 'ul', className: 'case-info', children: [
-        element({ type: 'li', className: 'case-project', content: `PROJECT: ${item.project}` }),
-        element({ type: 'li', className: 'case-project', content: `ROLE: ${item.role}` }),
-        element({ type: 'li', className: 'case-project', content: `DEMO: ${item.demo}` })
+      element({ type: 'div', className: 'case-info', children: [
+      element({ type: 'div', className: 'contentCase', children: [
+      element({ type: 'ul', className: 'case-roles', children: [
+        element({ type: 'li', className: 'case-project', content: `<b>PROJECT:</b> ${item.project}` }),
+        element({ type: 'li', className: 'case-project', content: `<b>ROLE:</b> ${item.role}` }),
+        element({ type: 'li', className: 'case-project', content: `<b>DEMO:</b> ${item.demo}` })
       ] }),
       element({ type: 'p', className: 'case-description', content: `${item.description}` })
+    ] }),
+    ] }),
     ]})
   ]});
 });
